ebb7d93af4da25e692867672a8a8d7f359df191e
中村 隆徳 <nakap@example.com>
2021-10-13 16:28:56 +0900

    flatbuffersのデータを扱うように修正しました、 出力先のデバッグコンソールのコードページを65001にしました。

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,13 @@
 ﻿#include <vector>
 #include <algorithm>
+#include <Windows.h>
+
+#include "ToolsAndData/MonsterData_generated.h"
+#include "flatbuffers/util.h"
+
+
+#define USE_FLATBUFFERS 1 
+
 
 /// モンスターデータ
 struct MonsterData
@@ -28,10 +36,10 @@ static constexpr MonsterData s_monsterList[] =
 // どのモンスターが強いかは、実際に戦わせて決めます。
 // 戦闘は 1 vs 1 で行い、先に力尽きた方が負けです。（力尽きる＝ヒットポイントが0以下になる）
 // ステータスの意味は以下の通りです。
-// ・hp      残り体力
-// ・ap   攻撃力（この値がそのまま基礎タメージ値になります）
+// ・hp  残体力
+// ・ap  攻撃力（この値がそのまま基礎タメージ値になります）
 // ・dp  防御力（この値の分だけ、受ける基礎ダメージの割合を減らします）
-// 例）攻撃側の atackPower が 20 で、防御側の防御力が 30 の時は、防御側は 20 の威力の攻撃を 30% 吸収するんで、 14 ダメージを受けます。
+// 例）攻撃側の ap が 20 で、防御側の dp が 30 の時は、防御側は 20 の威力の攻撃を 30% 吸収するんで、 14 ダメージを受けます。
 // ダメージ値に小数が出た場合は切り捨てられます。（例：1.5 ⇒ 1）
 // 2体は同時に戦います。同時に力尽きた時は、力尽きた時の体力が大きい方が勝ちです。（例：残り体力が -20 と -5になった場合は、-5の方が勝ち）
 
@@ -40,22 +48,23 @@ void Attack(MonsterData& offense, MonsterData& defense)
 {
     int damage = offense.ap * (100 - defense.dp) / 100;
     defense.hp -= damage;
-    printf("  %12s の攻撃！ %12s に %3d のダメージ！ 残りHP(%4d)\n", offense.name, defense.name, damage, defense.hp);
+    //printf("  %12s の攻撃！ %12s に %3d のダメージ！ 残りHP(%4d)\n", offense.name, defense.name, damage, defense.hp);
 }
 
 bool IsStronger(MonsterData& a, MonsterData& b)
 {
-    Attack(a, b);
-    Attack(b, a);
-
-    if (a.hp > 0 && b.hp > 0) IsStronger(a, b);
+    while (a.hp > 0 && b.hp > 0)
+    {
+        Attack(a, b);
+        Attack(b, a);
+    }
 
     return a.hp >= b.hp;
 }
 
 bool Battle(const MonsterData& a, const MonsterData& b)
 {
-    printf("%s vs %s\n", a.name, b.name);
+    //printf("%s vs %s\n", a.name, b.name);
 
     auto aa = a;
     auto bb = b;
@@ -64,12 +73,44 @@ bool Battle(const MonsterData& a, const MonsterData& b)
 
 int main()
 {
+#if defined(USE_FLATBUFFERS) && USE_FLATBUFFERS
+    // flatbuffersでバイナリにシリアライズされたデータを読み込んで、monsterListを構築する
+    std::vector<MonsterData> monsterList;
+    std::string binaryData;
+    if (flatbuffers::LoadFile("ToolsAndData/MonsterData.mdfb", true, &binaryData))
+    {
+        auto monsters = Data::GetMonsterList(binaryData.data());
+        MonsterData monster;
+        for (const auto& it : *monsters->data())
+        {
+            monster.label = it->label()->c_str();
+            monster.name = it->name()->c_str();
+            monster.hp = it->hp();
+            monster.ap = it->ap();
+            monster.dp = it->dp();
+            monsterList.push_back(monster);
+        }
+    }
+#else
     std::vector<MonsterData> monsterList(std::begin(s_monsterList), std::end(s_monsterList));
-    std::sort(monsterList.begin(), monsterList.end(), [](const auto& a, const auto& b) { return Battle(a, b); });
+#endif
 
+    SetConsoleOutputCP(65001);
+
+    // ソート前の状態を表示
     for (const auto& monster : monsterList)
     {
-        printf("%10s %12s : HP(%4d) ATK(%3d) DEF(%3d)\n", monster.label, monster.name, monster.hp, monster.ap, monster.dp);
+        printf("%10s %14s : HP(%4d) ATK(%3d) DEF(%3d)\n", monster.label, monster.name, monster.hp, monster.ap, monster.dp);
+    }
+    printf("\n");
+
+    // ソート
+    std::sort(monsterList.begin(), monsterList.end(), [](const auto& a, const auto& b) { return Battle(a, b); });
+
+    // ソート後の状態を表示
+    for (const auto& monster : monsterList)
+    {
+        printf("%10s %14s : HP(%4d) ATK(%3d) DEF(%3d)\n", monster.label, monster.name, monster.hp, monster.ap, monster.dp);
     }
     printf("\n");
 }
